4babdf8b285a687e44b399d98d6a25c949dccff0
Chris <ChrisNosowsky@gmail.com>
2020-05-12 21:45:54 -0400

    fixes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, logging
+import sys
 from app import pythonYoutube
 
 app = Flask(__name__)             # create an app instance
