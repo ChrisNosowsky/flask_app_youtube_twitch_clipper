375ea12db901cbb46b62b0c15c03a43367d2c8ce
Chris <ChrisNosowsky@gmail.com>
2020-05-27 21:13:23 -0400

    Executable file added

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for, session, logging
-import sys
+from flask import Flask, render_template, request, redirect, url_for, session
 import pythonYoutube
 import os
-import json
-# import boto3
+import sys
+from flaskwebgui import FlaskUI
+from time import sleep
 
-app = Flask(__name__)             # create an app instance
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+if getattr(sys, 'frozen', False):
+    template_folder = resource_path('templates')
+    static_folder = resource_path('static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
+
+
+# app = Flask(__name__)             # create an app instance
 app.secret_key = "hot potato prox potato"
+ui = FlaskUI(app)
 
 
 @app.route("/")                   # at the end point /
@@ -19,26 +35,30 @@ def index():                      # call method hello
 def index_post():
     link = request.form['link']
     error = None
-    if "https://" in link and "youtube.com" in link and "&t=" in link:
+    if "https://" in link and "you" in link and "?t=" in link:
         session['link'] = link
-        return redirect("http://3.23.209.43/submitted")
+        return redirect(url_for("submitted"))
     else:
-        error = "Invalid Link. Please make sure you provided a timestamp (&t=)"
+        error = "Invalid Link. Please make sure you provided a timestamp (?t=)"
         return render_template('index.html', error=error)
 
 
 @app.route('/submitted', methods=["GET", "POST"])
 def submitted():
-    return render_template("submitted.html", downloadPath=None)
+    return render_template("submitted.html")
 
 
 @app.route('/download', methods=["GET", "POST"])
 def download():
     link = session.get("link", None)
+    sleep(0.5)
     downloadPath = pythonYoutube.get_youtube(link)
-    done = "Download finished!" + link + downloadPath
-    return render_template("submitted.html", done=done, downloadPath=downloadPath)
+    if downloadPath:
+        done = "Download finished! Check your downloads folder"
+    else:
+        done = "ERROR. TRY AGAIN. PROBABLY SOME BULLSHIT REQUEST DELAY"
+
+    return render_template("submitted.html", done=done)
 
 
-if __name__ == '__main__':
-    app.run()
+ui.run()
--- pythonYoutube.py
+++ pythonYoutube.py
@@ -1,31 +1,35 @@
-import pytube
-from moviepy.editor import VideoClip, VideoFileClip
+from pytube import YouTube
+import subprocess
+import time
 import os
 
 
 def get_youtube(link):
     """Gets a YouTube link from the front-end and downloads it to users default download path"""
     try:
-        timestamp_index = link.find('&t=') + 3
+        timestamp_index = link.find('?t=') + 3
         timestamp = ""
         for i in range(timestamp_index, len(link)):
             if link[i].isdigit():
                 timestamp += link[i]
             else:
                 break
-        yt = pytube.YouTube(link)
+        time.sleep(1)
+        download_path = get_download_path()
+        time.sleep(3)
+        yt = YouTube(link)
         bounds = get_bounds(timestamp, yt)
-        yt.streams.first().download('./download_folder/')
+        yt.streams.first().download(download_path)
         filename = yt.streams.first().default_filename
-        full_path = './download_folder/' + filename
-
-        video = VideoFileClip(full_path)
-        video_cut = video.subclip(bounds[0], bounds[1])
-        video_cut.write_videofile(download_path + '\\' + "CLIPPED.mp4")
-        return "SUCCESS" + full_path
+        full_path = download_path + '\\' + filename
+        thetime = time.strftime('%H:%M:%S', time.gmtime(bounds[0]))
+        print(thetime)
+        command = "ffmpeg -y -i " + full_path + " -ss " + thetime + " -t 90 -vcodec copy -acodec copy " + download_path + '\\' + "CLIPPED.mp4"
+        subprocess.call(command, shell=True)
+        return True
     except:
         print("ERROR. PLEASE RETURN TO PREVIOUS SCREEN AND TRY AGAIN.")
-        return "ERROR" + link[:-5]
+        return False
 
 
 def get_bounds(timestamp, yt):
@@ -40,3 +44,16 @@ def get_bounds(timestamp, yt):
         upper_bound = int(timestamp) + 60
 
     return [lower_bound, upper_bound]
+
+
+def get_download_path():
+    """Returns the default downloads path for linux or windows"""
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'downloads')
